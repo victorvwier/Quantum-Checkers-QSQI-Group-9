7c43375d99d1a71495bed3bf710f013040f5f10a
mstyczen <42466994+mstyczen@users.noreply.github.com>
2021-01-18 17:02:05 +0100

    Visual and style improvements
    Change the way entangle/quantum mode buttons are displayed.
    Add icon for the game window.
    Change some code to follow the naming/style conventions.

--- QuantumCheckers/Constants.py
+++ QuantumCheckers/Constants.py
@@ -1,23 +1,25 @@
 
-
-Rows=4
-Cols=4
+Rows = 4
+Cols = 4
 Piece_Rows = 1
 
-Width,Height = 1000, 1000
-Square_Size = Width//Cols
+Width ,Height = 1000, 1000
+Square_Size = Width // Cols
 
-Red = (255,0,0)
-White = (255,255,255)
-Transparent_White = (255,255,255,128)
-Slightly_Transparent_White = (255,255,255,172)
-Transparent_Grey = (128,128,128,128)
+# colors
+Red = (255, 0, 0)
+White = (255, 255, 255)
+Gold = (239 ,235 ,14)
+Grey = (46, 46, 46)
+Green = (0 ,255 ,0)
+
+Transparent_White = (255, 255, 255, 128)
+Slightly_Transparent_White = (255 ,255 ,255 ,172)
+Transparent_Grey = (128 ,128 ,128 ,128)
 Blue_Piece = (26, 18, 112)
 Red_Piece = (112, 18, 18)
-Black = (0,0,0)
-Blue = (0,0,255)
+Black = (0 ,0 ,0)
+Blue = (0 ,0 ,255)
 Board_Brown = (241, 241, 241)
 Board_White = (163, 180, 216)
-Gold = (239,235,14)
-Grey = (46, 46, 46)
-Green = (0,255,0)
+
